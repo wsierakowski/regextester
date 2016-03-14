5aa553b0f368b6ed2a673d37f05844ec1a85e89c
wsierak <wojciech@sierakowski.eu>
2016-03-14 15:17:28 +0000

    Added share link feature

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const inRgxValDefault = "quick\\s(brown).*(jumps)";
 const inTxt = document.querySelector("#inTxt");
 const inRgx = document.querySelector("#inRgx");
 const runBtn = document.querySelector("#runBtn");
-const outTxt = window.document.querySelector("#outTxt");
-const outExec = window.document.querySelector("#outExec");
+const shareBtn = document.querySelector("#shareBtn");
+const outTxt = document.querySelector("#outTxt");
+const outExec = document.querySelector("#outExec");
+const shareLink = document.querySelector("#shareLink");
 const chGlob = document.querySelector("#chGlob");
 const chCase = document.querySelector("#chCase");
 const chMult = document.querySelector("#chMult");
 
 const runRx = () => {
+  getShareLink();
   var str = inTxt.value;
   if (str.length === 0) {outTxt.innerText = outExec.innerText = "";return;}
   if (inRgx.value.length === 0) {outTxt.innerText = outExec.innerText = "";return;}
@@ -68,12 +71,72 @@ const getQueryStringParams = () => {
   runRx();
 };
 
+const getShareLink = () => {
+  var link = location.protocol + '//' + location.host + location.pathname;
+  link += "?intxt=" + encodeURIComponent(inTxt.value);
+  link += "&inrgx" + encodeURIComponent(inRgx.value);
+  shareLink.value = link;
+}
+
 runBtn.onclick = inRgx.oninput = 
 chGlob.onchange = chCase.onchange = chMult.onchange = 
 inTxt.oninput = runRx;
 
+shareBtn.onclick = () => {copyToClipboard(shareLink);};
+
 window.onload = getQueryStringParams;
 
+//------------------------------------------------------------
+// http://stackoverflow.com/questions/22581345/click-button-copy-to-clipboard-using-jquery
+function copyToClipboard(elem) {
+	  // create hidden text element, if it doesn't already exist
+    var targetId = "_hiddenCopyText_";
+    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+    var origSelectionStart, origSelectionEnd;
+    if (isInput) {
+        // can just use the original source element for the selection and copy
+        target = elem;
+        origSelectionStart = elem.selectionStart;
+        origSelectionEnd = elem.selectionEnd;
+    } else {
+        // must use a temporary form element for the selection and copy
+        target = document.getElementById(targetId);
+        if (!target) {
+            var target = document.createElement("textarea");
+            target.style.position = "absolute";
+            target.style.left = "-9999px";
+            target.style.top = "0";
+            target.id = targetId;
+            document.body.appendChild(target);
+        }
+        target.textContent = elem.textContent;
+    }
+    // select the content
+    var currentFocus = document.activeElement;
+    target.focus();
+    target.setSelectionRange(0, target.value.length);
+    
+    // copy the selection
+    var succeed;
+    try {
+    	  succeed = document.execCommand("copy");
+    } catch(e) {
+        succeed = false;
+    }
+    // restore original focus
+    if (currentFocus && typeof currentFocus.focus === "function") {
+        currentFocus.focus();
+    }
+    
+    if (isInput) {
+        // restore prior selection
+        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+    } else {
+        // clear temporary content
+        target.textContent = "";
+    }
+    return succeed;
+}
 //------------------------------------------------------------
 // encode(decode) html text into html entity
 var decodeHtmlEntity = function(str) {
